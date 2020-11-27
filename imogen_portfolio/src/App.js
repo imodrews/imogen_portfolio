4767754c4463415c8e1966309fb0c711308eaff3
Imo <imoleadrews@gmail.com>
2020-11-27 17:15:48 +0100

    work bitch

--- imogen_portfolio/src/App.js
+++ imogen_portfolio/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import './App.css'
+import '../src/App.css';
 import { Switch, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
